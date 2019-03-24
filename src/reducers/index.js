b2fe671cafa9bf7c52f1a01dfc6acbb39b090446
Sixian Lin <davidlin006811@gmail.com>
2019-03-24 09:03:28 -0400

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import Auth from "./auth";
 import Products from "./products";
+import Messages from "./messages";
 export default combineReducers({
   auth: Auth,
-  products: Products
+  products: Products,
+  messages: Messages
 });
