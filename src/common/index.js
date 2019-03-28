28b48c932b25f0c96146690860c4b011a1d1feeb
Sixian Lin <davidlin006811@gmail.com>
2019-03-27 22:01:51 -0400

    add forgetPassword and resetPassword

--- src/common/index.js
+++ src/common/index.js
@@ -2,12 +2,19 @@ export { default as Loading } from "./Loading";
 export { default as isEmpty } from "./isEmpty";
 export { default as TextFieldGroup } from "./textField";
 export { default as TextAreaGroup } from "./textArea";
-export { default as forgotPassword } from "./validators/forgotPassword";
+export { default as forgotPassword } from "./validators/forgetPassword";
 export { default as loginValidation } from "./validators/login";
 export { default as productValidation } from "./validators/product";
 export { default as careerValidation } from "./validators/career";
 export { default as Modal } from "./modal";
 export { default as getFileExtension } from "./fileType";
+export { default as themeValidation } from "./validators/theme";
 export {
   default as changePasswordValidataion
 } from "./validators/changePassword";
+export {
+  default as forgetPasswordValidataion
+} from "./validators/forgetPassword";
+export {
+  default as resetPasswordValidataion
+} from "./validators/resetPassword";
